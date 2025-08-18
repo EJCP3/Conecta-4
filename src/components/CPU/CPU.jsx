4a2e2bbe53009e91e9620da7084b34a060f115e0
EJCP3 <euddycabrera03@gmail.com>
2025-08-18 12:42:48 -0400

    v4.1.2

--- src/components/CPU/CPU.jsx
+++ src/components/CPU/CPU.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "../Player/Header";
-import Main from "../Player/Main";
-import { useUpdateBoard } from "../../hooks/useUpdateBoard";
+// import React from "react";
+// import Header from "../Player/Header";
+// import Main from "../Player/Main";
+// import { useUpdateBoard } from "../../hooks/useUpdateBoard";
 
-export default function CPU() {
-  const hookData = useUpdateBoard();
-  return (
-    <div className="w-full h-dvh">
-      <Header hookData={hookData} />
-      <Main hookData={hookData} />
-    </div>
-  );
-}
+// export default function CPU() {
+//   const hookData = useUpdateBoard();
+//   return (
+//     <div className="w-full h-dvh">
+//       <Header hookData={hookData} />
+//       <Main hookData={hookData} />
+//     </div>
+//   );
+// }
